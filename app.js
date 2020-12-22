7bf3fff40bfd118cc4fcad254ef1254d4e30bbee
longphamk2kk <71912566+longphamk2kk@users.noreply.github.com>
2020-12-22 23:27:05 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
-let a1 = prompt("nhap mang 1").split(",")
-let a2 = prompt('nhap mang 2').split(",")
-
-function phan_tu(a1, a2) {
-
-    let a = [],
-        a3 = []
-
-    for (let i = 0; i < a1.length; i++) {
-        a[a1[i]] = true;
+const endpoint = "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
+dulieu = fetch(endpoint).then((response) => response.json()).then((data) => {
+    console.log(data)
+    for (let i = 0; i < data.results.length; i++) {
+        console.log(data.results[i])
+        let rawhtml = ` <center style="border: 20px solid pink;">
+        <h3>Question:${i+1}</h3>
+        <div class="question">${data.results[i].question}</div>
+        <div><input type="radio" name="answers${i}" value="${data.results[i].incorrect_answers[0]}">A.<span>${data.results[i].incorrect_answers[0]}</span></div>
+        <div><input type="radio" name="answers${i}" value="${data.results[i].correct_answer}">B.<span>${data.results[i].correct_answer}</span></div>
+        <div><input type="radio" name="answers${i}" value="${data.results[i].incorrect_answers[1]}">C.<span>${data.results[i].incorrect_answers[1]}</span></div>
+        <div><input type="radio" name="answers${i}" value="${data.results[i].incorrect_answers[2]}">D.<span>${data.results[i].incorrect_answers[2]}</span></div>
+        <div style="display: flex;justify-content: center;">
+            <div class="category" style="margin: 50px;">Category: Sports</div>
+            <div class="difficulty" style="margin: 50px;">Difficulty: Eassy</div>
+        </div>
+        <div class="error${i}"></div>
+        
+    </center>`
+        document.querySelector(".app").insertAdjacentHTML("beforeend", rawhtml)
     }
 
-    for (let i = 0; i < a2.length; i++) {
-        if (a[a2[i]]) {
-            delete a[a2[i]];
-        } else {
-            a[a2[i]] = true;
+    let answers = document.querySelector(".form")
+    document.querySelector(".submit").addEventListener("click", (event) => {
+        event.preventDefault()
+        let count = 0
+        for (let i = 0; i < data.results.length; i++) {
+            let a = "answers.answers" + i + ".value"
+            console.log(a)
+            answer = eval(a)
+            console.log(answer)
+            if (answer == data.results[i].correct_answer) {
+                count++
+                document.querySelector(".error" + i).innerHTML = "<p style='color:green;'>TRUE</p>"
+            } else {
+                document.querySelector(".error" + i).innerHTML = "<p style='color:red;'>FALSE</p>"
+            }
         }
-    }
-
-    for (let k in a) {
-        a3.push(k);
-    }
-
-    return a3;
-}
-phan_tu(a1, a2)
-console.log(phan_tu(a1, a2));
+        alert("CORRECT ANSWER : " + count + " Question")
+        document.querySelector(".result").textContent = "TRUE    " + count + "/" + data.results.length
+    })
+})
